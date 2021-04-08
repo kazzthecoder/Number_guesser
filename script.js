07ea9ea930918bcab2ddcf2be2954bc12497b7dd
Kazz Bryan <kazz.bryan@gmail.com>
2021-04-08 16:45:45 -0400

    Add files via upload
    fix

--- script.js
+++ script.js
@@ -2,32 +2,22 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
-
-let humanScore = 0;
-let computerScore = 0;
-let currentRoundNumber = 1;
-
-// Write your code below:
 const generateTarget = () => {
-    return Math.floor(Math.random() * 10);
-  }
-  
+  return Math.floor(Math.random() * 10);
+}
+
 const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
-    const humanDifference = Math.abs(targetGuess - humanGuess)
-    const computerDifference = Math.abs(targetGuess - computerGuess)
-    return humanDifference <= computerDifference;
-  }
-  
+  const humanDifference = Math.abs(targetGuess - humanGuess)
+  const computerDifference = Math.abs(targetGuess - computerGuess)
+  return humanDifference <= computerDifference;
+}
+
 const updateScore = winner => {
-    switch(winner) {
-    case 'human':
-      humanScore += 1;
-      break;
-    case 'computer': 
-      computerScore += 1;
-      break;
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
   }
-  }
-  
+}
+
 const advanceRound = () => currentRoundNumber++;
